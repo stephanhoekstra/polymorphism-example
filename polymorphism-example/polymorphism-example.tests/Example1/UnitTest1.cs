8cd976603589c32ff15186c7224b69388e2a39f8
Stephan Hoekstra <stephan@fre-w8ws-098.funda.nl>
2016-03-09 16:34:30 +0100

    Billy eats a banana

---
+++ polymorphism-example/polymorphism-example.tests/Example1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using polymorphism_example.Example1;
+
+namespace polymorphism_example.tests.Example1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BillyEatsPizzaAndBanana_AssertBelly()
+        {
+            Person billy = new Person();
+            billy.Eat(new Banana());
+            billy.Eat(new Pizza());
+
+            var actual = billy.WhatDoIHave();
+            Console.WriteLine(actual);
+        }
+    }
+}
---
+++ polymorphism-example/polymorphism-example/Example1/Example1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism_example.Example1
+{
+    public abstract class Food
+    {
+        public  abstract string Describe();
+    }
+
+    public class Pizza : Food
+    {
+        public override string Describe()
+        {
+            return "a pizza";
+        }
+    }
+
+    public class Banana : Food
+    {
+        public override string Describe()
+        {
+            return "an unpealed banana";
+        }
+    }
+
+    public class Person
+    {
+        private readonly IList<Food> _belly;
+
+        public Person()
+        {
+            _belly = new List<Food>();
+        }
+
+        public void Eat(Food food)
+        {
+            _belly.Add(food);
+        }
+
+        public string WhatDoIHave()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("What's in my belly:");
+            foreach (var food in _belly)
+            {
+                sb.AppendLine(food.Describe());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
